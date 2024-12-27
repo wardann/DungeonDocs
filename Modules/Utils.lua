ad5241efa823ebdd8f66926afa46b8b3e2610f99
wardann <185875474+wardann@users.noreply.github.com>
2024-12-26 22:44:15 -0500

    Rearrange utils file

--- Modules/Utils.lua
+++ Modules/Utils.lua
@@ -3,6 +3,13 @@ local DD = LibStub("AceAddon-3.0"):GetAddon("DungeonDocs")
 local AceGUI = LibStub("AceGUI-3.0") ---@type AceGUI
 local M = {}
 
+-- #    # ###### #      #####  ###### #####   ####
+-- #    # #      #      #    # #      #    # #
+-- ###### #####  #      #    # #####  #    #  ####
+-- #    # #      #      #####  #      #####       #
+-- #    # #      #      #      #      #   #  #    #
+-- #    # ###### ###### #      ###### #    #  ####
+
 ---@param t table
 ---@param indent number
 function M.InspectTable(t, indent)
@@ -113,11 +120,210 @@ function M.IsFollowerNPC(mobID)
 	return false
 end
 
---   __
---  / _|_ __ __ _ _ __ ___   ___
--- | |_| '__/ _` | '_ ` _ \ / _ \
--- |  _| | | (_| | | | | | |  __/
--- |_| |_|  \__,_|_| |_| |_|\___|
+---@param active PlayerNotes
+---@param fallback PlayerNotes
+---@return PlayerNotes
+function M.MergePlayerNotes(active, fallback)
+	---@alias PlayerNotesMapped table<DungeonName, table<DDID, PlayerNote>>
+	local playerNotesMapped = {} ---@type PlayerNotesMapped
+
+	for dungeonName, notes in pairs(active) do
+		if not playerNotesMapped[dungeonName] then
+			playerNotesMapped[dungeonName] = {}
+		end
+
+		for _, note in ipairs(notes) do
+			playerNotesMapped[dungeonName][note.ddid] = note
+		end
+	end
+
+	-- Add the notes from the fallback profile where applicable
+	for instance, fallbackNotes in pairs(fallback) do
+		if not playerNotesMapped[instance] then
+			playerNotesMapped[instance] = {}
+		end
+
+		for _, fallbackNote in ipairs(fallbackNotes) do
+			(function()
+				local found = playerNotesMapped[instance][fallbackNote.ddid]
+				if not found then
+					playerNotesMapped[instance][fallbackNote.ddid] = fallbackNote
+					return
+				end
+
+				---@param activeValue string
+				---@param fallbackValue string
+				local function shouldUseFallback(activeValue, fallbackValue)
+					return not activeValue and fallbackValue and #fallbackValue > 0
+				end
+
+				if shouldUseFallback(found.primaryNote, fallbackNote.primaryNote) then
+					found.primaryNote = fallbackNote.primaryNote
+				end
+
+				if shouldUseFallback(found.tankNote, fallbackNote.tankNote) then
+					found.tankNote = fallbackNote.tankNote
+				end
+
+				if shouldUseFallback(found.healerNote, fallbackNote.healerNote) then
+					found.healerNote = fallbackNote.healerNote
+				end
+
+				if shouldUseFallback(found.damageNote, fallbackNote.damageNote) then
+					found.damageNote = fallbackNote.damageNote
+				end
+			end)()
+		end
+	end
+
+	local playerNotesFinal = {} ---@type PlayerNotes
+
+	for dungeonName, notes in pairs(playerNotesMapped) do
+		for _, note in pairs(notes) do
+			if not playerNotesFinal[dungeonName] then
+				playerNotesFinal[dungeonName] = {}
+			end
+			table.insert(playerNotesFinal[dungeonName], note)
+		end
+	end
+
+	return playerNotesFinal
+end
+
+---@param tbl table
+function M.IsTableEmpty(tbl)
+	return next(tbl) == nil
+end
+
+---@param array any[]
+---@param element any
+function M.IsInArray(array, element)
+	for _, e in ipairs(array) do
+		if e == element then
+			return true
+		end
+	end
+	return false
+end
+
+---@param tbl table<any, any>
+---@param indent string?
+---@return string
+function M.TableToString(tbl, indent)
+	indent = indent or ""
+	local result = "{\n"
+	local nextIndent = indent .. "  "
+
+	for k, v in pairs(tbl) do
+		local key = tostring(k)
+		if type(v) == "table" then
+			result = result .. nextIndent .. key .. " = " .. M.TableToString(v, nextIndent) .. ",\n"
+		else
+			result = result .. nextIndent .. key .. " = " .. tostring(v) .. ",\n"
+		end
+	end
+
+	return result .. indent .. "}"
+end
+
+-- Function to log the human-readable table string to BugSack
+---@param msg string
+---@param tbl table<any, any>
+function M.LogTableToBugSack(msg, tbl)
+	local readableTable = M.TableToString(tbl)
+	error(msg .. "\n\n" .. readableTable) -- Triggers BugSack to capture the output
+end
+
+---@param message string
+function M.LogToGroup(message)
+	---@param msg string
+	---@param channel string
+	local function sendMultiLineMsg(msg, channel)
+		for line in msg:gmatch("[^\n]+") do
+			SendChatMessage(line, channel)
+		end
+	end
+
+	if IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
+		-- Player is in an instance group (e.g., dungeon finder or raid finder)
+		sendMultiLineMsg(message, "INSTANCE_CHAT")
+	elseif IsInGroup(LE_PARTY_CATEGORY_HOME) then
+		-- Player is in a regular party or raid group
+		sendMultiLineMsg(message, "PARTY")
+	else
+		sendMultiLineMsg(message, "SAY")
+	end
+end
+
+--- Wraps text in a light gray color escape sequence for logging
+--- @param text string The text to wrap
+--- @return string The text wrapped in a light gray color
+function M.Gray(text)
+	return "|cffd3d3d3" .. text .. "|r"
+end
+
+-- ######  ####  #    # #####  ####
+-- #      #    # ##   #   #   #
+-- #####  #    # # #  #   #    ####
+-- #      #    # #  # #   #        #
+-- #      #    # #   ##   #   #    #
+-- #       ####  #    #   #    ####
+
+local LSM = LibStub("LibSharedMedia-3.0") ---@type any
+local fontList = LSM:HashTable("font") ---@type table<string, string>
+
+local fontNameToPath = fontList
+local fontPathToName = {} ---@type table<string, string>
+for fontName, fontPath in pairs(fontList) do
+	fontPathToName[fontPath] = fontName -- Use font names as display text
+end
+
+local fontNames = {} ---@type table<string, string>
+for fontName, _ in pairs(fontList) do
+	fontNames[fontName] = fontName -- Use font names as display text
+end
+
+---@param fontName string
+function M.FontNameToPath(fontName)
+	return fontNameToPath[fontName]
+end
+
+---@param fontPath string
+function M.FontPathToName(fontPath)
+	return fontPathToName[fontPath]
+end
+
+---@param container AceGUIContainer
+---@param label string
+---@param startingFont string
+---@param callback fun(string)
+function M.AddFontSelect(container, label, startingFont, callback)
+	-- Dropdown menu for font selection
+	local fontDropdown = AceGUI:Create("Dropdown") ---@type Dropdown
+	fontDropdown:SetLabel(label)
+
+	-- Create a list with font-specific labels
+	fontDropdown:SetList(fontNames)
+
+	-- Set initial font
+	fontDropdown:SetValue(startingFont)
+
+	-- Set callback to apply the selected font and preview it
+	fontDropdown:SetCallback("OnValueChanged", function(_, _, selectedFont)
+		callback(selectedFont)
+	end)
+
+	container:AddChild(fontDropdown)
+end
+
+DD.utils = M
+
+-- ###### #####    ##   #    # ######
+-- #      #    #  #  #  ##  ## #
+-- #####  #    # #    # # ## # #####
+-- #      #####  ###### #    # #
+-- #      #   #  #    # #    # #
+-- #      #    # #    # #    # ######
 
 ---@param frame Frame|FontString
 function M.FrameHide(frame)
@@ -283,200 +489,3 @@ function M.SafeSetColorTexture(texture, r, g, b, a)
 		texture:SetColorTexture(r, g, b, a)
 	end
 end
-
----@param active PlayerNotes
----@param fallback PlayerNotes
----@return PlayerNotes
-function M.MergePlayerNotes(active, fallback)
-	---@alias PlayerNotesMapped table<DungeonName, table<DDID, PlayerNote>>
-	local playerNotesMapped = {} ---@type PlayerNotesMapped
-
-	for dungeonName, notes in pairs(active) do
-		if not playerNotesMapped[dungeonName] then
-			playerNotesMapped[dungeonName] = {}
-		end
-
-		for _, note in ipairs(notes) do
-			playerNotesMapped[dungeonName][note.ddid] = note
-		end
-	end
-
-	-- Add the notes from the fallback profile where applicable
-	for instance, fallbackNotes in pairs(fallback) do
-		if not playerNotesMapped[instance] then
-			playerNotesMapped[instance] = {}
-		end
-
-		for _, fallbackNote in ipairs(fallbackNotes) do
-			(function()
-				local found = playerNotesMapped[instance][fallbackNote.ddid]
-				if not found then
-					playerNotesMapped[instance][fallbackNote.ddid] = fallbackNote
-					return
-				end
-
-				---@param activeValue string
-				---@param fallbackValue string
-				local function shouldUseFallback(activeValue, fallbackValue)
-					return not activeValue and fallbackValue and #fallbackValue > 0
-				end
-
-				if shouldUseFallback(found.primaryNote, fallbackNote.primaryNote) then
-					found.primaryNote = fallbackNote.primaryNote
-				end
-
-				if shouldUseFallback(found.tankNote, fallbackNote.tankNote) then
-					found.tankNote = fallbackNote.tankNote
-				end
-
-				if shouldUseFallback(found.healerNote, fallbackNote.healerNote) then
-					found.healerNote = fallbackNote.healerNote
-				end
-
-				if shouldUseFallback(found.damageNote, fallbackNote.damageNote) then
-					found.damageNote = fallbackNote.damageNote
-				end
-			end)()
-		end
-	end
-
-	local playerNotesFinal = {} ---@type PlayerNotes
-
-	for dungeonName, notes in pairs(playerNotesMapped) do
-		for _, note in pairs(notes) do
-			if not playerNotesFinal[dungeonName] then
-				playerNotesFinal[dungeonName] = {}
-			end
-			table.insert(playerNotesFinal[dungeonName], note)
-		end
-	end
-
-	return playerNotesFinal
-end
-
----@param tbl table
-function M.IsTableEmpty(tbl)
-	return next(tbl) == nil
-end
-
----@param array any[]
----@param element any
-function M.IsInArray(array, element)
-	for _, e in ipairs(array) do
-		if e == element then
-			return true
-		end
-	end
-	return false
-end
-
----@param tbl table<any, any>
----@param indent string?
----@return string
-function M.TableToString(tbl, indent)
-	indent = indent or ""
-	local result = "{\n"
-	local nextIndent = indent .. "  "
-
-	for k, v in pairs(tbl) do
-		local key = tostring(k)
-		if type(v) == "table" then
-			result = result .. nextIndent .. key .. " = " .. M.TableToString(v, nextIndent) .. ",\n"
-		else
-			result = result .. nextIndent .. key .. " = " .. tostring(v) .. ",\n"
-		end
-	end
-
-	return result .. indent .. "}"
-end
-
--- Function to log the human-readable table string to BugSack
----@param msg string
----@param tbl table<any, any>
-function M.LogTableToBugSack(msg, tbl)
-	local readableTable = M.TableToString(tbl)
-	error(msg .. "\n\n" .. readableTable) -- Triggers BugSack to capture the output
-end
-
----@param message string
-function M.LogToGroup(message)
-	---@param msg string
-	---@param channel string
-	local function sendMultiLineMsg(msg, channel)
-		for line in msg:gmatch("[^\n]+") do
-			SendChatMessage(line, channel)
-		end
-	end
-
-	if IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
-		-- Player is in an instance group (e.g., dungeon finder or raid finder)
-		sendMultiLineMsg(message, "INSTANCE_CHAT")
-	elseif IsInGroup(LE_PARTY_CATEGORY_HOME) then
-		-- Player is in a regular party or raid group
-		sendMultiLineMsg(message, "PARTY")
-	else
-		sendMultiLineMsg(message, "SAY")
-	end
-end
-
---- Wraps text in a light gray color escape sequence for logging
---- @param text string The text to wrap
---- @return string The text wrapped in a light gray color
-function M.Gray(text)
-    return "|cffd3d3d3" .. text .. "|r"
-end
-
---   __             _
---  / _| ___  _ __ | |_ ___
--- | |_ / _ \| '_ \| __/ __|
--- |  _| (_) | | | | |_\__ \
--- |_|  \___/|_| |_|\__|___/
-
-local LSM = LibStub("LibSharedMedia-3.0") ---@type any
-local fontList = LSM:HashTable("font") ---@type table<string, string>
-
-local fontNameToPath = fontList
-local fontPathToName = {} ---@type table<string, string>
-for fontName, fontPath in pairs(fontList) do
-	fontPathToName[fontPath] = fontName -- Use font names as display text
-end
-
-local fontNames = {} ---@type table<string, string>
-for fontName, _ in pairs(fontList) do
-	fontNames[fontName] = fontName -- Use font names as display text
-end
-
----@param fontName string
-function M.FontNameToPath(fontName)
-	return fontNameToPath[fontName]
-end
-
----@param fontPath string
-function M.FontPathToName(fontPath)
-	return fontPathToName[fontPath]
-end
-
----@param container AceGUIContainer
----@param label string
----@param startingFont string
----@param callback fun(string)
-function M.AddFontSelect(container, label, startingFont, callback)
-	-- Dropdown menu for font selection
-	local fontDropdown = AceGUI:Create("Dropdown") ---@type Dropdown
-	fontDropdown:SetLabel(label)
-
-	-- Create a list with font-specific labels
-	fontDropdown:SetList(fontNames)
-
-	-- Set initial font
-	fontDropdown:SetValue(startingFont)
-
-	-- Set callback to apply the selected font and preview it
-	fontDropdown:SetCallback("OnValueChanged", function(_, _, selectedFont)
-		callback(selectedFont)
-	end)
-
-	container:AddChild(fontDropdown)
-end
-
-DD.utils = M
