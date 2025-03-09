5490d1e1bd6277f88fb870b7f9c33b5c6307b635
wardann <185875474+wardann@users.noreply.github.com>
2025-03-09 13:25:11 -0400

    Add S2 dungeons

---
+++ Dungeons/TWW/S2/CinderbrewMeadery.lua
@@ -0,0 +1,296 @@
+
+---@type DungeonDocs
+local DD = LibStub("AceAddon-3.0"):GetAddon("DungeonDocs")
+
+---@type CreateDDID
+local function createDDID(index)
+	return "cbm" .. index
+end
+
+---@class Dungeons
+DD.dungeons = DD.dungeons or {}
+
+---@type DocStructure[]
+local docStructures = {
+	{
+		docName = "Brewmaster Aldryr",
+		ddid = createDDID(1),
+		isBoss = true,
+		mobs = {
+			{
+				-- name = "Brewmaster Aldryr",
+				id = "210271",
+				displayId = 116429,
+			},
+		},
+	},
+	{
+		docName = "I'pa",
+		ddid = createDDID(2),
+		isBoss = true,
+		mobs = {
+			{
+				-- name = "I'pa",
+				id = "210267",
+				displayId = 117177,
+			},
+		},
+	},
+	{
+		docName = "Benk Buzzbee",
+		ddid = createDDID(3),
+		isBoss = true,
+		mobs = {
+			{
+				-- name = "Benk Buzzbee",
+				id = "218002",
+				displayId = 120557,
+			},
+		},
+	},
+	{
+		docName = "Goldie Baronbottom",
+		ddid = createDDID(4),
+		isBoss = true,
+		mobs = {
+			{
+				-- name = "Goldie Baronbottom",
+				id = "214661",
+				displayId = 118000,
+			},
+		},
+	},
+	{
+		docName = "Tasting Room Attendant",
+		ddid = createDDID(5),
+		mobs = {
+			{
+				-- name = "Tasting Room Attendant",
+				id = "214920",
+				displayId = 121037,
+			},
+		},
+	},
+	{
+		docName = "Venture Co. Pyromaniac",
+		ddid = createDDID(6),
+		mobs = {
+			{
+				-- name = "Venture Co. Pyromaniac",
+				id = "218671",
+				displayId = 119013,
+			},
+		},
+	},
+	{
+		docName = "Venture Co. Patron",
+		ddid = createDDID(7),
+		mobs = {
+			{
+				-- name = "Venture Co. Patron",
+				id = "214668",
+				displayId = 119026,
+			},
+		},
+	},
+	{
+		docName = "Hired Muscle",
+		ddid = createDDID(8),
+		mobs = {
+			{
+				-- name = "Hired Muscle",
+				id = "210269",
+				displayId = 118999,
+			},
+		},
+	},
+	{
+		docName = "Chef Chewie",
+		ddid = createDDID(9),
+		mobs = {
+			{
+				-- name = "Chef Chewie",
+				id = "214697",
+				displayId = 121036,
+			},
+		},
+	},
+	{
+		docName = "Worker Bee",
+		ddid = createDDID(10),
+		mobs = {
+			{
+				-- name = "Worker Bee",
+				id = "210265",
+				displayId = 121054,
+			},
+		},
+	},
+	{
+		docName = "Royal Jelly Purveyor",
+		ddid = createDDID(11),
+		mobs = {
+			{
+				-- name = "Royal Jelly Purveyor",
+				id = "220141",
+				displayId = 118378,
+			},
+		},
+	},
+	{
+		docName = "Venture Co. Honey Harvester",
+		ddid = createDDID(12),
+		mobs = {
+			{
+				-- name = "Venture Co. Honey Harvester",
+				id = "220946",
+				displayId = 108585,
+			},
+		},
+	},
+	{
+		docName = "Bee Wrangler",
+		ddid = createDDID(13),
+		mobs = {
+			{
+				-- name = "Bee Wrangler",
+				id = "210264",
+				displayId = 118076,
+			},
+		},
+	},
+	{
+		docName = "Bee-let",
+		ddid = createDDID(14),
+		mobs = {
+			{
+				-- name = "Bee-let",
+				id = "218865",
+				displayId = 121060,
+			},
+		},
+	},
+	{
+		docName = "Flavor Scientist",
+		ddid = createDDID(15),
+		mobs = {
+			{
+				-- name = "Flavor Scientist",
+				id = "214673",
+				displayId = 119436,
+			},
+		},
+	},
+	{
+		docName = "Taste Tester",
+		ddid = createDDID(16),
+		mobs = {
+			{
+				-- name = "Taste Tester",
+				id = "220060",
+				displayId = 118003,
+			},
+		},
+	},
+	{
+		docName = "Careless Hopgoblin",
+		ddid = createDDID(17),
+		mobs = {
+			{
+				-- name = "Careless Hopgoblin",
+				id = "223423",
+				displayId = 121087,
+			},
+		},
+	},
+	{
+		docName = "Brew Drop",
+		ddid = createDDID(18),
+		mobs = {
+			{
+				-- name = "Brew Drop",
+				id = "210270",
+				displayId = 117828,
+			},
+		},
+	},
+	{
+		docName = "Yes Man",
+		ddid = createDDID(19),
+		mobs = {
+			{
+				-- name = "Yes Man",
+				id = "219588",
+				displayId = 121109,
+			},
+		},
+	},
+	{
+		docName = "Flavor Scientist",
+		ddid = createDDID(20),
+		mobs = {
+			{
+				-- name = "Flavor Scientist",
+				id = "222964",
+				displayId = 119436,
+			},
+		},
+	},
+	{
+		docName = "Brew Drop",
+		ddid = createDDID(21),
+		mobs = {
+			{
+				-- name = "Brew Drop",
+				id = "219301",
+				displayId = 120498,
+			},
+		},
+	},
+	{
+		docName = "Ravenous Cinderbee",
+		ddid = createDDID(22),
+		mobs = {
+			{
+				-- name = "Ravenous Cinderbee",
+				id = "218016",
+				displayId = 120631,
+			},
+		},
+	},
+	{
+		docName = "Worker Bee",
+		ddid = createDDID(23),
+		mobs = {
+			{
+				-- name = "Worker Bee",
+				id = "223497",
+				displayId = 121054,
+			},
+		},
+	},
+	{
+		docName = "Bee-let",
+		ddid = createDDID(24),
+		mobs = {
+			{
+				-- name = "Bee-let",
+				id = "223498",
+				displayId = 121060,
+			},
+		},
+	},
+
+}
+
+---@type DungeonData
+local dungeonData = {
+	dungeonName = "Cinderbrew Meadery",
+	icon = "Interface\\Icons\\inv_misc_web_01",
+	seasonId = "TWWS2",
+	docStructures = docStructures,
+}
+
+function DD.dungeons.InitCinderbrewMeadery()
+	DD.dungeons.List[dungeonData.dungeonName] = dungeonData
+end
---
+++ Dungeons/TWW/S2/OperationFloodgate.lua
@@ -0,0 +1,308 @@
+
+---@type DungeonDocs
+local DD = LibStub("AceAddon-3.0"):GetAddon("DungeonDocs")
+
+---@type CreateDDID
+local function createDDID(index)
+	return "of" .. index
+end
+
+---@class Dungeons
+DD.dungeons = DD.dungeons or {}
+
+---@type DocStructure[]
+local docStructures = {
+	{
+		docName = "Big M.O.M.M.A.",
+		ddid = createDDID(1),
+		isBoss = true,
+		mobs = {
+			{
+				-- name = "Big M.O.M.M.A.",
+				id = "226398",
+				displayId = 122867,
+			},
+		},
+	},
+	{
+		docName = "Bront",
+		ddid = createDDID(2),
+		isBoss = true,
+		mobs = {
+			{
+				-- name = "Bront",
+				id = "226402",
+				displayId = 122215,
+			},
+		},
+	},
+	{
+		docName = "Keeza Quickfuse",
+		ddid = createDDID(3),
+		isBoss = true,
+		mobs = {
+			{
+				-- name = "Keeza Quickfuse",
+				id = "226403",
+				displayId = 123036,
+			},
+		},
+	},
+	{
+		docName = "Swampface",
+		ddid = createDDID(4),
+		isBoss = true,
+		mobs = {
+			{
+				-- name = "Swampface",
+				id = "226396",
+				displayId = 124283,
+			},
+		},
+	},
+	{
+		docName = "Geezle Gigazap",
+		ddid = createDDID(5),
+		isBoss = true,
+		mobs = {
+			{
+				-- name = "Geezle Gigazap",
+				id = "226404",
+				displayId = 122475,
+			},
+		},
+	},
+	{
+		docName = "Shreddinator 3000",
+		ddid = createDDID(6),
+		mobs = {
+			{
+				-- name = "Shreddinator 3000",
+				id = "230740",
+				displayId = 122881,
+			},
+		},
+	},
+	{
+		docName = "Darkfuse Soldier",
+		ddid = createDDID(7),
+		mobs = {
+			{
+				-- name = "Darkfuse Soldier",
+				id = "228144",
+				displayId = 122752,
+			},
+		},
+	},
+	{
+		docName = "Mechadrone Sniper",
+		ddid = createDDID(8),
+		mobs = {
+			{
+				-- name = "Mechadrone Sniper",
+				id = "229069",
+				displayId = 126613,
+			},
+		},
+	},
+	{
+		docName = "Loaderbot",
+		ddid = createDDID(9),
+		mobs = {
+			{
+				-- name = "Loaderbot",
+				id = "231014",
+				displayId = 121696,
+			},
+		},
+	},
+	{
+		docName = "Venture Co. Contractor",
+		ddid = createDDID(10),
+		mobs = {
+			{
+				-- name = "Venture Co. Contractor",
+				id = "229250",
+				displayId = 122082,
+			},
+		},
+	},
+	{
+		docName = "Darkfuse Hyena",
+		ddid = createDDID(11),
+		mobs = {
+			{
+				-- name = "Darkfuse Hyena",
+				id = "229252",
+				displayId = 121293,
+			},
+		},
+	},
+	{
+		docName = "Darkfuse Demolitionist",
+		ddid = createDDID(12),
+		mobs = {
+			{
+				-- name = "Darkfuse Demolitionist",
+				id = "229212",
+				displayId = 124724,
+			},
+		},
+	},
+	{
+		docName = "Darkfuse Bloodwarper",
+		ddid = createDDID(13),
+		mobs = {
+			{
+				-- name = "Darkfuse Bloodwarper",
+				id = "230748",
+				displayId = 126383,
+			},
+		},
+	},
+	{
+		docName = "Darkfuse Inspector",
+		ddid = createDDID(14),
+		mobs = {
+			{
+				-- name = "Darkfuse Inspector",
+				id = "231385",
+				displayId = 124177,
+			},
+		},
+	},
+	{
+		docName = "Venture Co. Surveyor",
+		ddid = createDDID(15),
+		mobs = {
+			{
+				-- name = "Venture Co. Surveyor",
+				id = "229686",
+				displayId = 122080,
+			},
+		},
+	},
+	{
+		docName = "Venture Co. Electrician",
+		ddid = createDDID(16),
+		mobs = {
+			{
+				-- name = "Venture Co. Electrician",
+				id = "231312",
+				displayId = 124181,
+			},
+		},
+	},
+	{
+		docName = "Bombshell Crab",
+		ddid = createDDID(17),
+		mobs = {
+			{
+				-- name = "Bombshell Crab",
+				id = "231497",
+				displayId = 120779,
+			},
+		},
+	},
+	{
+		docName = "Venture Co. Diver",
+		ddid = createDDID(18),
+		mobs = {
+			{
+				-- name = "Venture Co. Diver",
+				id = "231496",
+				displayId = 122887,
+			},
+		},
+	},
+	{
+		docName = "Venture Co. Architect",
+		ddid = createDDID(19),
+		mobs = {
+			{
+				-- name = "Venture Co. Architect",
+				id = "229251",
+				displayId = 122097,
+			},
+		},
+	},
+	{
+		docName = "Disturbed Kelp",
+		ddid = createDDID(20),
+		mobs = {
+			{
+				-- name = "Disturbed Kelp",
+				id = "231223",
+				displayId = 126961,
+			},
+		},
+	},
+	{
+		docName = "Bubbles",
+		ddid = createDDID(21),
+		mobs = {
+			{
+				-- name = "Bubbles",
+				id = "231197",
+				displayId = 124782,
+			},
+		},
+	},
+	{
+		docName = "Darkfuse Jumpstarter",
+		ddid = createDDID(22),
+		mobs = {
+			{
+				-- name = "Darkfuse Jumpstarter",
+				id = "231325",
+				displayId = 122517,
+			},
+		},
+	},
+	{
+		docName = "Darkfuse Mechadrone",
+		ddid = createDDID(23),
+		mobs = {
+			{
+				-- name = "Darkfuse Mechadrone",
+				id = "228424",
+				displayId = 125461,
+			},
+		},
+	},
+	{
+		docName = "Undercrawler",
+		ddid = createDDID(24),
+		mobs = {
+			{
+				-- name = "Undercrawler",
+				id = "231380",
+				displayId = 112449,
+			},
+		},
+	},
+	{
+		docName = "Waterworks Crocolisk",
+		ddid = createDDID(25),
+		mobs = {
+			{
+				-- name = "Waterworks Crocolisk",
+				id = "227145",
+				displayId = 75850,
+			},
+		},
+	},
+
+}
+
+---@type DungeonData
+local dungeonData = {
+	dungeonName = "Operation: Floodgate",
+	icon = "Interface\\Icons\\inv_misc_web_01",
+	seasonId = "TWWS2",
+	docStructures = docStructures,
+}
+
+function DD.dungeons.InitOperationFloodgate()
+	DD.dungeons.List[dungeonData.dungeonName] = dungeonData
+end
